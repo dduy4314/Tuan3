b8fb28a3cc6e5a48da93d56f7edd6249d234d555
dduy4314 <153704896+dduy4314@users.noreply.github.com>
2023-12-25 14:58:35 +0700

    T32Adapter.java

---
+++ T32Adapter.java
@@ -0,0 +1,70 @@
+package com.example.tuan11myapplication.tuan3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tuan11myapplication.R;
+
+import java.util.ArrayList;
+
+public class T32Adapter extends BaseAdapter {
+    private ArrayList<T32Contact> ls;
+    private Context context;
+
+    public T32Adapter(ArrayList<T32Contact> ls, Context context) {
+        this.ls = ls;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return ls.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return ls.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewAX vax;
+
+        if(convertView==null){
+            vax=new ViewAX();
+
+            convertView= LayoutInflater.from(context).inflate(R.layout.tuan32_listview_item,null);
+
+            vax.img_hinh=convertView.findViewById(R.id.t32ItemHinh);
+            vax.tv_ten=convertView.findViewById(R.id.t32ItemTen);
+            vax.tv_tuoi=convertView.findViewById(R.id.t32ItemTuoi);
+
+            convertView.setTag(vax);
+
+        }
+        else {
+            vax=(ViewAX) convertView.getTag();
+
+        }
+
+        vax.img_hinh.setImageResource(ls.get(position).getHinh());
+        vax.tv_ten.setText(ls.get(position).getTen());
+        vax.tv_tuoi.setText(ls.get(position).getTuoi());
+        return convertView;
+    }
+    class ViewAX{
+        ImageView img_hinh;
+        TextView tv_ten, tv_tuoi;
+
+    }
+}
